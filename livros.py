5f08e661351b7e018e64558169f5dbaa56e43d35
DyeffersonAz <dyeffersonazevedo@hotmail.com>
2021-03-29 19:15:38 -0300

    Adding custom start

--- livros.py
+++ livros.py
@@ -6,16 +6,19 @@ from math import ceil
 
 #input do usuario
 paginas = int(input("Quantas páginas/capítulos tem o livro? "))
+try:
+    inicio = int(input("Em qual página/capítulo você vai iniciar o livro? "))
+except ValueError:
+    inicio = 0
 dias = int(input("Em quantos dias quer lê-lo? "))
-paginasDiarias = ceil(paginas/dias)
+paginasDiarias = ceil((paginas-inicio)/dias)
 
 #apos calcular o quanto de páginas o usuario deverá ler por dia o programa arrendonda pra cima e mostra o resultado
 print("\nVocê vai ler {} páginas/capítulos por dia!\n".format(paginasDiarias))
 
 #loop pra mostrar cada dia e as páginas sucessórias
 for dia in range(1, dias+1):
-    if paginasDiarias*dia >= paginas:
-        print("Dia {}: Você termina o livro lendo até a página/capítulo {}".format(dia, paginas))
-        break
-    else:
-        print("Dia {}: ler até página/capítulo {}".format(dia, paginasDiarias*dia))
+    ate = (dia*paginasDiarias)+inicio
+    if ate > paginas:
+        ate = paginas
+    print(f"Dia {dia}: Você vai até a página/capítulo {ate};")
